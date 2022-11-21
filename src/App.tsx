863913634a496a51b97939b0e3cd7028d954c72f
Aleksandr Nekrasov <n.alexsergeevich@gmail.com>
2022-11-21 17:50:59 +0300

    Added header, pages, routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Layout } from './components/Layout/Layout';
+import { Albums } from './pages/Albums/Albums';
+import { Posts } from './pages/Posts/Posts';
+import { Todos } from './pages/Todos/Todos';
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Layout>
+          <Routes>
+            <Route index element={<Posts />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/albums" element={<Albums />} />
+            <Route path="/todos" element={<Todos />} />
+          </Routes>
+        </Layout>
+      </div>
+    </BrowserRouter>
+  );
 };
